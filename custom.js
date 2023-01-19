20d6aa016042819a06a545dac3c576c8cf7a6bb6
Shadipto Mojumder <118707478+shadipto58@users.noreply.github.com>
2023-01-19 23:52:39 +0600

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,19 @@
+
+
+function box() {
+	alert("We Are Learning JS");
+}
+
+var offer = document.getElementById("jam");
+
+offer.style.color = "#273c75";
+
+offer.innerHTML = "Traffic jam in Dhaka";
+
+ var apple = document.getElementById("button");
+ button.style.color = "#00a8ff";
+
+
+var abc = "HOW are you?";
+
+alert(abc);
